6a39caf28193a0ac237b4076a4a16325644b95ce
lara huang <lara1105huang@gmail.com>
2025-04-17 18:54:14 +0800

    設定環境變數

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
+import { onMounted } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
+onMounted(() => {
+  console.log('頁面使用:', import.meta.env.VITE_API_BASIC_URL)
+})
 </script>
 
 <template>
@@ -13,18 +17,3 @@ import HelloWorld from './components/HelloWorld.vue'
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
